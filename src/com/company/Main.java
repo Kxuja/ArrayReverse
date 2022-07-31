bd322a2a82fc59cfb1b74ced1dacef4a2544513a
Kxuja <Kxujadeveloper@gmail.com>
2022-07-31 06:03:08 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+public class Main {
+    public static void main(String[] args) {
+        array7(new int[]{1,2,3,4,5,6,7,8});
+    }
+    public static void array7(int[] array) {
+
+        for (int i = array.length-1; i >= 0; i--) {
+            System.out.print(array[i]+"\t");
+
+
+        }
+    }
+}
